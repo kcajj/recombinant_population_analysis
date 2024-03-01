34eb64fa653bc4cfb05f6860115d85a2f91a8657
jack <jack@Ubuntu22.04.myguest.virtualbox.org>
2024-03-01 12:17:08 +0100

    dealing with arrays of recombination evidences

--- notes/todo.md
+++ notes/todo.md
@@ -3,5 +3,9 @@
         [x] generate data on the basis of given probabilities
         [] understand viterbi script deeply
         [x] make predictions on that data
+    [] test on Aionostat data
+        [] clones
+            [x] extract evidences from a clone sequence
+            [] make a prediction with the viterbi algorithm
 
 
--- scripts/hmm.py
+++ scripts/hmm.py
@@ -1,49 +1,7 @@
+from Bio import AlignIO
 import numpy as np
-
-'''
-generate sequences on the basis of given probabilties
-'''
-
-n=100
-
-initial_p={"A":0.5,"B":0.5}
-
-transition_p_froma={"A":0.99,"B":0.01}
-transition_p_fromb={"A":0.01,"B":0.99}
-
-emission_p_froma={"a":0.40,"b":0.01,".":0.59}
-emission_p_fromb={"a":0.01,"b":0.40,".":0.59}
-
-hid_seq=np.random.choice(list(initial_p.keys()), p=list(initial_p.values()))
-
-for i in range(n):
-    if hid_seq[-1]=="A":
-        step=np.random.choice(list(transition_p_froma.keys()), p=list(transition_p_froma.values()))
-        hid_seq+=step
-    else:
-        step=np.random.choice(list(transition_p_fromb.keys()), p=list(transition_p_fromb.values()))
-        hid_seq+=step
-
-print(hid_seq)
-
-#hid_seq="BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-
-emission_seq=""
-
-for hidden_state in hid_seq:
-    if hidden_state=="A":
-        emission=np.random.choice(list(emission_p_froma.keys()), p=list(emission_p_froma.values()))
-        emission_seq+=emission
-    else:
-        emission=np.random.choice(list(emission_p_fromb.keys()), p=list(emission_p_fromb.values()))
-        emission_seq+=emission
-
-print(emission_seq)
-#emission_seq="bb...b......bb.b.b.b...bb.bb....b.baa...a.a..aaabaaaa.aaa....aa.aa....a.aa..a......a.a.aaa.........aa"
-
-'''
-test viterbi algorithm
-'''
+import matplotlib.pyplot as plt
+from handle_msa import read_msa, get_evidences_distributions
 
 def viterbi(y, A, B, pi):
     """
@@ -73,23 +31,14 @@ def viterbi(y, A, B, pi):
         i -= 1
         prev_ind = x_seq[int(prev_ind), i]
     return x_seq_opt
+    
+initial_p={"A":0.5,"B":0.5}
 
-hidden_index=[]
-for h in hid_seq:
-    if h=="A":
-        hidden_index.append(0)
-    else:
-        hidden_index.append(1)
-emission_index=[]
-for e in emission_seq:
-    if e=="a":
-        emission_index.append(0)
-    elif e=="b":
-        emission_index.append(1)
-    else:
-        emission_index.append(2)
+transition_p_froma={"A":0.99,"B":0.01}
+transition_p_fromb={"A":0.01,"B":0.99}
 
-print(emission_index)
+emission_p_froma={".":0.599,"a":0.40,"b":0.001}
+emission_p_fromb={".":0.599,"a":0.001,"b":0.40}
 
 ip_np=np.array(list(initial_p.values()))
 tp_np=np.array([list(transition_p_froma.values()),
@@ -97,13 +46,29 @@ tp_np=np.array([list(transition_p_froma.values()),
 ep_np=np.array([list(emission_p_froma.values()),
                 list(emission_p_fromb.values())])
 
-o=viterbi(emission_index, tp_np, ep_np, ip_np)
+'''
+takes a MSA, extracts the evidences (handle msa) and gives them in input to viterbi algorithm
+'''
 
-predicted_hidden_index = o.tolist()
-predicted_hidden_index = [int(x) for x in predicted_hidden_index]
+if __name__ == "__main__":
 
-print(hidden_index)
-print(predicted_hidden_index)
+    populations=['P2','P3']
+    clones=['C1','C2','C3','C4']
+    references=['EM11','EM60']
 
-accuracy = sum([1 for i in range(len(hidden_index)) if hidden_index[i] == predicted_hidden_index[i]]) / len(hidden_index)
-print(accuracy)
+    for population in populations:
+        for clone in clones:
+
+            file=f'results/msa/clones/{population}_{clone}_msa.fasta'
+            out_folder=f'results/plots/recombination_evidences/clones/{population}_{clone}_msa.png'
+
+            k=1000 #convolution window
+
+            msa_matrix=read_msa(file)
+
+            e_distribution = get_evidences_distributions(msa_matrix)
+            
+            o = viterbi(e_distribution, tp_np, ep_np, ip_np)
+
+            print(o)
+
