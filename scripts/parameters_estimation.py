49ded084ebed405ea5197440f40ae6ffca2d6481
kcajj <thejack2307@gmail.com>
2024-03-06 20:30:58 +0100

    parameters estimation with adjustdirectionaccurately

--- scripts/parameters_estimation.py
+++ scripts/parameters_estimation.py
@@ -4,6 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import subprocess
 from handle_msa import read_msa, get_evidences_distributions
+import time
+from collections import defaultdict
 
 '''
 
@@ -16,26 +18,21 @@ phages=['EM11','EM60']
 
 refs_msa_path="results/msa/refs_msa.fasta"
 
+null_prob=defaultdict(list)
+a_prob=defaultdict(list)
+b_prob=defaultdict(list)
+time_spent=defaultdict(list)
+
 for phage in phages:
     
     temp_fasta_path = f"results/temp/{phage}_read.fasta"
     temp_output_path = f"results/temp/{phage}_read_msa.fasta"
-
-    null_prob=[]
-    a_prob=[]
-    b_prob=[]
     
     c = 0
     with gzip.open(f"data/{phage}_new_chemistry.fastq.gz", "rt") as handle:
         for record in SeqIO.parse(handle, "fastq"):
-            
             # we need to align the read to the refs_msa.fasta file and extract the evidences
-
-            
-            # mafft --auto 
-            #       --addfragments results/seq_for_msa/${population}/${timepoint}/${population}_${timepoint}_${read}.fasta
-            #       --keeplength results/msa/refs_msa.fasta
-            #       > results/msa/${population}/${timepoint}/${population}_${timepoint}_${read}_msa.fasta
+            start_time=time.time()
 
             # Create a temporary fasta file with the id and sequence of the read
             with open(temp_fasta_path, "w") as temp_fasta:
@@ -43,7 +40,7 @@ for phage in phages:
                 temp_fasta.close()
 
             # create the alignment
-            msa_command = f"mafft --auto --adjustdirection --addfragments {temp_fasta_path} {refs_msa_path} > {temp_output_path}"
+            msa_command = f"mafft --auto --adjustdirectionaccurately --addfragments {temp_fasta_path} {refs_msa_path} > {temp_output_path}"
             subprocess.run(msa_command, shell=True)
 
             msa_matrix = read_msa(temp_output_path)
@@ -51,23 +48,42 @@ for phage in phages:
             e_distribution = get_evidences_distributions(msa_matrix,i_ref1=0,i_ref2=1,i_extra=2)
 
             l = len(e_distribution)
-            null_prob.append(np.count_nonzero(e_distribution == 0)/l)
-            a_prob.append(np.count_nonzero(e_distribution == 1)/l)
-            b_prob.append(np.count_nonzero(e_distribution == 2)/l)
+            null_prob[phage].append(np.count_nonzero(e_distribution == 0)/l)
+            a_prob[phage].append(np.count_nonzero(e_distribution == 1)/l)
+            b_prob[phage].append(np.count_nonzero(e_distribution == 2)/l)
+            end_time=time.time()
+            time_spent[phage].append(end_time-start_time)
 
             plt.scatter(range(len(e_distribution)), e_distribution, c=e_distribution, marker='|', alpha=0.5)
             plt.title('evidence distribution (purple no evidence, green evidence for A, yellow evidence for B)')
 
-            plt.show()
+            plt.show(block=False)
+            time.sleep(1)
+            plt.close()
 
             #remove temporary files
             rm_command = f"rm {temp_fasta_path} {temp_output_path}"
             subprocess.run(rm_command, shell=True)
             
             c+=1
-            if c==5:break
-    
-    mean_null_prob = np.mean(null_prob)
-    mean_a_prob = np.mean(a_prob)
-    mean_b_prob = np.mean(b_prob)
-    print(f"phage {phage} null_prob: {mean_null_prob} a_prob: {mean_a_prob} b_prob: {mean_b_prob}")
+            if c==100:break
+
+print("mean null probability")
+for k,v in null_prob.items():
+    print(k," ",np.mean(v))
+print("")
+
+print("mean a probability")
+for k,v in a_prob.items():
+    print(k," ",np.mean(v))
+print("")
+
+print("mean b probability")
+for k,v in b_prob.items():
+    print(k," ",np.mean(v))
+print("")
+
+print("mean time spent")
+for k,v in time_spent.items():
+    print(k," ",np.mean(v))
+print("")
