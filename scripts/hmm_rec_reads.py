bc80b192f2731915a3640e8706d10917f54ccebe
jack <jack@Ubuntu22.04.myguest.virtualbox.org>
2024-03-28 09:12:29 +0100

    analysis on whole dataset corrected

--- scripts/hmm_rec_reads.py
+++ scripts/hmm_rec_reads.py
@@ -33,11 +33,11 @@ if __name__ == "__main__":
 
     refs_msa_path="results/msa/refs_msa.fasta"
 
-    #bam_file=f"results/alignments/{population}_{timestep}.bam"
-    bam_file=f"data/test/hybrid_test_{population}_{timestep}.bam" #for test dataset
+    bam_file=f"results/alignments/{population}_{timestep}.bam"
+    #bam_file=f"data/test/hybrid_test_{population}_{timestep}.bam" #for test dataset
 
-    #output_path=f"results/genomewide_recombination_arrays/{population}_{timestep}.npz"
-    output_path=f"results/genomewide_recombination_arrays/test_{population}_{timestep}.npz" #for test dataset
+    output_path=f"results/genomewide_recombination_arrays/{population}_{timestep}.npz"
+    #output_path=f"results/genomewide_recombination_arrays/test_{population}_{timestep}.npz" #for test dataset
 
     length_treshold=5000 #just for the total dataset
 
@@ -57,67 +57,67 @@ if __name__ == "__main__":
 
                 l_read=read.infer_read_length()
                 l_alignment=read.query_alignment_length
-                #if l_alignment>length_treshold: #just for the total dataset
+                if l_alignment>length_treshold: #just for the total dataset
 
-                mapping_start=read.reference_start
-                mapping_end=read.reference_end
+                    mapping_start=read.reference_start
+                    mapping_end=read.reference_end
 
-                start_time=time.time()
+                    start_time=time.time()
 
-                alignment_array = read.get_aligned_pairs()
+                    alignment_array = read.get_aligned_pairs()
 
-                for (read_pos,ref_pos) in alignment_array:
-                    if ref_pos!=None: #clips and gaps ar
-                        if read_pos==None:
-                            read_msa_seq+='-'
-                        else:
-                            read_msa_seq+=read_seq[read_pos].lower()
+                    for (read_pos,ref_pos) in alignment_array:
+                        if ref_pos!=None: #clips and gaps ar
+                            if read_pos==None:
+                                read_msa_seq+='-'
+                            else:
+                                read_msa_seq+=read_seq[read_pos].lower()
 
-                msa_matrix = add_to_msa(refs_msa_path, read_msa_seq, mapping_start, mapping_end)
+                    msa_matrix = add_to_msa(refs_msa_path, read_msa_seq, mapping_start, mapping_end)
 
-                e_distribution_to_plot = get_evidences_distributions(msa_matrix)
+                    e_distribution_to_plot = get_evidences_distributions(msa_matrix)
 
-                e_distribution = np.where(e_distribution_to_plot > 0, e_distribution_to_plot-1, e_distribution_to_plot)
+                    e_distribution = np.where(e_distribution_to_plot > 0, e_distribution_to_plot-1, e_distribution_to_plot)
 
-                hmm_prediction = viterbi_algorithm(e_distribution, tp_np, ep_np, ip_np)
+                    hmm_prediction = viterbi_algorithm(e_distribution, tp_np, ep_np, ip_np)
 
-                pre_status = hmm_prediction[0]
-                for i in range(len(hmm_prediction)):
-                    post_status = hmm_prediction[i]
-                    if pre_status != post_status:
-                        recombination_distribution[mapping_start+i] += 1
-                    pre_status = post_status
+                    pre_status = hmm_prediction[0]
+                    for i in range(len(hmm_prediction)):
+                        post_status = hmm_prediction[i]
+                        if pre_status != post_status:
+                            recombination_distribution[mapping_start+i] += 1
+                        pre_status = post_status
 
-                end_time=time.time()
-                time_spent_per_read[population].append(end_time-start_time)
-                time_spent_per_base[population].append((end_time-start_time)/len(hmm_prediction))
+                    end_time=time.time()
+                    time_spent_per_read[population].append(end_time-start_time)
+                    time_spent_per_base[population].append((end_time-start_time)/len(hmm_prediction))
 
-                print(c)
-                c1+=1
+                    print(c)
+                    c1+=1
 
-                '''
-                #to create a plot for each read
-                plot_path = f"results/plots/reads/{population}_{timestep}_{c}.png"
+                    '''
+                    #to create a plot for each read
+                    plot_path = f"results/plots/reads/{population}_{timestep}_{c}.png"
 
-                hmm_plot, (evidences, prediction) = plt.subplots(2, 1, figsize=(10, 5))
-                hmm_plot.suptitle(f'HMM read {c}, {population}, t{timestep}')
+                    hmm_plot, (evidences, prediction) = plt.subplots(2, 1, figsize=(10, 5))
+                    hmm_plot.suptitle(f'HMM read {c}, {population}, t{timestep}')
 
-                colours = np.where(e_distribution_to_plot == 0, "green", np.where(e_distribution_to_plot == 1, "red", np.where(e_distribution_to_plot == 2, "orange", "blue")))
-                evidences.scatter(range(mapping_start,len(e_distribution_to_plot)+mapping_start), e_distribution_to_plot, c=colours, marker='|', alpha=0.5)
-                evidences.set_title('evidence distribution (0:same, 1:err, 2:a, 3:b)')
-                evidences.set_xlabel("basepair")
-                evidences.set_ylabel("visible states")
+                    colours = np.where(e_distribution_to_plot == 0, "green", np.where(e_distribution_to_plot == 1, "red", np.where(e_distribution_to_plot == 2, "orange", "blue")))
+                    evidences.scatter(range(mapping_start,len(e_distribution_to_plot)+mapping_start), e_distribution_to_plot, c=colours, marker='|', alpha=0.5)
+                    evidences.set_title('evidence distribution (0:same, 1:err, 2:a, 3:b)')
+                    evidences.set_xlabel("basepair")
+                    evidences.set_ylabel("visible states")
 
-                colours = np.where(hmm_prediction == 0, "orange", "blue")
-                prediction.scatter(range(mapping_start,len(e_distribution_to_plot)+mapping_start), hmm_prediction, c=colours, marker='|', alpha=0.5)
-                prediction.set_title(f'HMM prediction (0:A, 1:B)')
-                prediction.set_xlabel("basepair")
-                prediction.set_ylabel("hidden states")
+                    colours = np.where(hmm_prediction == 0, "orange", "blue")
+                    prediction.scatter(range(mapping_start,len(e_distribution_to_plot)+mapping_start), hmm_prediction, c=colours, marker='|', alpha=0.5)
+                    prediction.set_title(f'HMM prediction (0:A, 1:B)')
+                    prediction.set_xlabel("basepair")
+                    prediction.set_ylabel("hidden states")
 
-                hmm_plot.tight_layout()
-                hmm_plot.savefig(plot_path)
-                plt.close(hmm_plot)
-                '''
+                    hmm_plot.tight_layout()
+                    hmm_plot.savefig(plot_path)
+                    plt.close(hmm_plot)
+                    '''
 
             c+=1
             if c%1000==0:
